77f07dc5f036764069a463ffd0b27feff5a28455
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-24 15:19:57 +0530

    Add method to count the number of alive neighbours

--- src/main/kotlin/GridState.kt
+++ src/main/kotlin/GridState.kt
@@ -1,4 +1,8 @@
 data class GridState(val liveCells: List<Cell>) {
     fun isCellAlive(cell: Cell)
         = liveCells.firstOrNull { it ==  cell} != null
+
+    fun getAliveNeighbourCountOf(cell: Cell): Int {
+        return cell.getAllNeighbourCells().count { isCellAlive(it) }
+    }
 }
--- src/test/kotlin/GridStateTest.kt
+++ src/test/kotlin/GridStateTest.kt
@@ -30,4 +30,14 @@ class GridStateTest {
         val expectedResult = false
         assertEquals(expectedResult, actualResult)
     }
+
+    @Test
+    fun `it should return the count of alive neighbours`() {
+        val gridState = GridState(liveCells = listOf(Cell(1, 1), Cell(2, 2), Cell(0, 2)))
+
+        val actualCount = gridState.getAliveNeighbourCountOf(Cell(2, 2))
+
+        val expectedCount = 1
+        assertEquals(expectedCount, actualCount)
+    }
 }
