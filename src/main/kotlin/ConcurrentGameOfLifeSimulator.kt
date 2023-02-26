4aa28fc4474607c0c40c0abb084c5de44f5df087
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-26 23:20:56 +0530

    Add concurrent Game of Life Simulator

---
+++ src/main/kotlin/ConcurrentGameOfLifeSimulator.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+
+class ConcurrentGameOfLifeSimulator : GameOfLifeSimulator() {
+    override fun getNextState(previousGridState: GridState): GridState {
+        return GridState(
+            liveCells = runBlocking { runTask(previousGridState, previousGridState.liveCells).toList() }
+        )
+    }
+
+    private suspend fun runTask(state: GridState, list: List<Cell>): Set<Cell> = coroutineScope {
+        if (list.size < 500) {
+            return@coroutineScope getAllCandidateForNextGenAliveCells(prevGenLiveCells = list)
+                .filter { isCellAliveInNextGen(state, it) }
+                .toSet()
+        }
+
+        val m = list.size / 2
+
+        val leftList = list.subList(0, m + 1)
+        val coroutineOne = async(Dispatchers.Default) { runTask(state, leftList) }
+        coroutineOne.start()
+
+        val rightList = list.subList(m + 1, list.size)
+        val coroutineTwo = async(Dispatchers.Default) { runTask(state, rightList) }
+        coroutineTwo.start()
+
+        return@coroutineScope merge(coroutineOne.await(), coroutineTwo.await())
+    }
+
+    private fun merge(a: Set<Cell>, b: Set<Cell>) = a.plus(b)
+}
